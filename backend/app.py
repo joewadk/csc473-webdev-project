2b68a581d0e3b959df54418372342d99ce3bcb78
joewadk <jkabir000@citymail.cuny.edu>
2024-11-15 18:04:18 -0500

    added comments

--- backend/app.py
+++ backend/app.py
@@ -3,21 +3,20 @@ from flask_cors import CORS
 from supabase import create_client, Client
 import os
 from dotenv import load_dotenv
-import json
 from typing import List
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '../frontend/.env'))
 
-app = Flask(__name__)
+app = Flask(__name__) #intiate the flask app
 CORS(app)
 
-# Initialize Supabase client
+#env stuff
 supabase_url = os.getenv("VITE_SUPABASE_URL")
 supabase_key = os.getenv("VITE_SUPABASE_ANON_KEY")
 
-supabase: Client = create_client(supabase_url, supabase_key)
+supabase: Client = create_client(supabase_url, supabase_key) #create the supabase client
 
-@app.route('/courses', methods=['GET'])
+@app.route('/courses', methods=['GET']) #route to get all courses
 def get_courses():
     try:
         response = supabase.table('courses').select('*').execute()
@@ -26,5 +25,5 @@ def get_courses():
     except Exception:
         return jsonify({"error": "Can't find courses"}),500
 
-if __name__ == '__main__':
+if __name__ == '__main__': #main
     app.run(port=5000, debug=True)
